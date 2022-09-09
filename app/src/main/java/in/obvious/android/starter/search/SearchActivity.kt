476bd9b13802b629810e70e08a0d4f1070d61505
Rishab <rishab@obvious.in>
2022-09-09 10:33:27 +0530

    Search UI
    - make search activity implement SearchUI

--- app/src/main/java/in/obvious/android/starter/search/SearchActivity.kt
+++ app/src/main/java/in/obvious/android/starter/search/SearchActivity.kt
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.spotify.mobius.Mobius
 import com.spotify.mobius.android.MobiusAndroid
 import `in`.obvious.android.starter.databinding.ActivitySearchBinding
+import android.view.View
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), SearchUI {
 
   private val searchUpdate = SearchUpdate()
   private val searchEffectHandler = SearchEffectHandler()
@@ -21,5 +22,32 @@ class SearchActivity : AppCompatActivity() {
   private fun startMobiusLoop() {
     val searchLoopFactory = Mobius.loop(searchUpdate, searchEffectHandler)
     val mobiusController = MobiusAndroid.controller(searchLoopFactory, SearchModel().initial())
+  }
+
+  override fun hideLoader() {
+    binding.searchProgress.visibility = View.GONE
+  }
+
+  override fun hideResults() {
+    binding.results.visibility = View.GONE
+  }
+
+  override fun hideError() {
+    binding.errorMessage.visibility = View.GONE
+  }
+
+  override fun showLoader(query: String) {
+    binding.searchProgress.visibility = View.VISIBLE
+  }
+
+  override fun showResults(results: List<String>) {
+    binding.results.apply {
+      visibility = View.VISIBLE
+      this.text = results.first()
+    }
+  }
+
+  override fun showError(message: String) {
+    binding.errorMessage.visibility = View.VISIBLE
   }
 }
